b77a0870828675c90ec8aabda7941b75d037e574
tve3 <vladu4eg@mail.ru>
2020-11-05 17:43:46 +0300

    private1

--- game/trollnelves2/scripts/vscripts/flag.lua
+++ game/trollnelves2/scripts/vscripts/flag.lua
@@ -8,7 +8,7 @@ function FlagStart(eventSourceIndex, event)
 		if casterHeroID:IsElf() and hero:IsElf() and PlayerResource:GetConnectionState(event.target) == 2 and GameRules:GetGameTime() - GameRules.startTime > 1 and GameRules.PlayersBase[event.casterID] ~= nil then	
 			CustomGameEventManager:Send_ServerToPlayer(PlayerResource:GetPlayer(event.target), "show_flag_options", {["name"] = playerName, ["id"] = event.target,["casterID"] = event.casterID} )
 			DebugPrint("FlagStart SEND")
-		elseif GameRules.PlayersBase[event.casterID] == nil then 
+			elseif GameRules.PlayersBase[event.casterID] == nil then 
 			SendErrorMessage(event.casterID, "You have no bases.")
 		end
 	end	
@@ -16,12 +16,22 @@ end
 
 function FlagGive(eventSourceIndex, event)
 	DebugPrint("event.vote " .. event.vote)
+	local hero = PlayerResource:GetSelectedHeroEntity(event.playerID1)
 	if event.vote == 1 then
         DebugPrint("GameRules.PlayersBase[event.casterID] FlagGive " .. GameRules.PlayersBase[event.casterID])
         DebugPrint("event.casterID FlagGive " .. event.casterID)
 		GameRules.PlayersBase[event.playerID1] = GameRules.PlayersBase[event.casterID]
+		if hero.units ~= nil then
+			for i=1,#hero.units do
+				if hero.units[i] and not hero.units[i]:IsNull() and hero.units[i]:GetUnitName() == "flag" then
+					local unit = hero.units[i]
+					unit:ForceKill(false)
+				end
+			end
+		end
+		hero:RemoveAbility("flag")
 	else
-		text = PlayerResource:GetPlayerName(event.target) .. " canceled the request for a private base."
-		GameRules:SendCustomMessageToTeam("<font color='#FF0000'>"  .. text  .. "</font>" , team, 0, 0)
-	end
+	text = PlayerResource:GetPlayerName(event.target) .. " canceled the request for a private base."
+	GameRules:SendCustomMessageToTeam("<font color='#FF0000'>"  .. text  .. "</font>" , team, 0, 0)
+end
 end
