b65a02e6958795123a6ba541fc982f84081a5567
tve3 <vladu4eg@mail.ru>
2021-05-04 02:40:33 +0300

    Update flag.lua

--- game/trollnelves2/scripts/vscripts/flag.lua
+++ game/trollnelves2/scripts/vscripts/flag.lua
@@ -10,7 +10,8 @@ function FlagStart(eventSourceIndex, event)
 		local playerName = PlayerResource:GetPlayerName(event.casterID)
 		if casterHero:IsElf() and hero:IsElf() and PlayerResource:GetConnectionState(event.target) == 2 and GameRules.PlayersBase[event.casterID] ~= nil and countFlag[event.casterID] == nil
 			and (GameRules:GetGameTime() - GameRules.startTime < 120 
-			or  (lastFlagTime[event.target] == nil or lastFlagTime[event.target] + 240 < GameRules:GetGameTime()) ) then	
+			or  (lastFlagTime[event.target] == nil or lastFlagTime[event.target] + 240 < GameRules:GetGameTime()) )
+			and (lastFlagTime[event.caster] == nil or lastFlagTime[event.caster] + 40 < GameRules:GetGameTime()) then	
 			CustomGameEventManager:Send_ServerToPlayer(PlayerResource:GetPlayer(event.target), "show_flag_options", {["name"] = playerName, ["id"] = event.target,["casterID"] = event.casterID} )
 			DebugPrint("FlagStart SEND")
 			if GameRules:GetGameTime() - GameRules.startTime > 120 then
@@ -20,6 +21,8 @@ function FlagStart(eventSourceIndex, event)
 			SendErrorMessage(event.casterID, "You have no bases.")
 		elseif countFlag[event.casterID] ~= nil then
 			SendErrorMessage(event.casterID, "You can only invite 1 person.")
+		else 
+			SendErrorMessage(event.casterID, "You can't send an invitation so often.")
 		end
 	end	
 end
