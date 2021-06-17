885048ba09453d1e94d640c0f685815912f44d86
tve3 <vladu4eg@mail.ru>
2021-06-17 18:38:40 +0300

    fix clave

--- game/trollnelves2/scripts/vscripts/heroes/hero_lone_druid/spirit_bear.lua
+++ game/trollnelves2/scripts/vscripts/heroes/hero_lone_druid/spirit_bear.lua
@@ -77,6 +77,7 @@ function SpiritBearSpawn( event )
 			caster.bear:RemoveAbility("attack_gold_wisp")
 			caster.bear:RemoveAbility("reveal_area")
 			caster.bear:RemoveAbility("troll_teleport")
+			caster.bear:RemoveModifierByName("gold_wisp_on_attack")
 		end
 		
 		else 
@@ -125,6 +126,7 @@ function SpiritBearLevel( event )
 		caster.bear:RemoveAbility("attack_gold_wisp")
 		caster.bear:RemoveAbility("reveal_area")
 		caster.bear:RemoveAbility("troll_teleport")
+		caster.bear:RemoveModifierByName("gold_wisp_on_attack")
 	end
 end
 
